b63d634074dc0e783a661f1aa947b67add99d413
AJ Arriola <angelojames.arriola@gmail.com>
2019-05-17 01:44:54 -0700

    2 added test for candidates and added struct mapping and addCandidate function to Election contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,28 @@
 pragma solidity >=0.4.22 <0.7.0;
 
 contract Election {
-  // Store candidate
-  // Read candidate
-  string public candidate;
-  // Constructor
-  constructor () public {
-    candidate = "Candidate1";
+  // Model a Candidate
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
   }
+
+  // Store Candidates
+  // Fetch Candidate
+  mapping(uint => Candidate) public candidates;
+
+  // Store Candidates Count
+  uint public candidatesCount;
+
+  constructor () public {
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate (string memory _name) private {
+    candidatesCount ++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
 }
